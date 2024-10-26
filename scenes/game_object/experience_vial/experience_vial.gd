5efac14222261e513ebfb634715d58face51df2e
Zachary Pickel <zpickel@gmail.com>
2024-10-26 16:22:14 -0500

    Initial commit

---
+++ scenes/game_object/experience_vial/experience_vial.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var sprite: Sprite2D = $Sprite2D
+
+
+func _ready() -> void:
+	$Area2D.area_entered.connect(on_area_entered)
+	
+	
+func tween_collect(percent: float, start_position: Vector2):
+	var player = get_tree().get_first_node_in_group("player")
+	if player == null:
+		return
+		
+	global_position = start_position.lerp(player.global_position, percent)
+	var direction_from_start = player.global_position - start_position
+	
+	var target_rotation = direction_from_start.angle() + deg_to_rad(90)
+	rotation_degrees = lerp_angle(rotation, target_rotation, 1 - exp(-2 * get_process_delta_time()))
+
+
+func collect():
+	GameEvents.emit_experience_vial_collected(1)
+	queue_free()
+
+
+func disable_collision():
+	collision_shape_2d.disabled = true
+
+func on_area_entered(other_area: Area2D):
+	Callable(disable_collision).call_deferred()
+	
+	var tween = create_tween()
+	
+	tween.set_parallel() # sets all tweens to run in parallel
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 0.5)\
+	.set_ease(Tween.EASE_IN)\
+	.set_trans(Tween.TRANS_BACK)
+	tween.tween_property(sprite, "scale", Vector2.ZERO, .05).set_delay(.45)
+	tween.chain() # will wait on previous tweens before calling later ones (like tween_callback)
+	
+	tween.tween_callback(collect)
